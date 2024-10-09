ae7ec3d9fa2738a10a5c911b7af4df348affe4c8
Morongwe Molaba <133514542+rongwes@users.noreply.github.com>
2024-10-09 03:18:19 +0000

    Added holidays for everyone and need inspiration section

--- src/Pages/Holidays/Holidays.jsx
+++ src/Pages/Holidays/Holidays.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Holidays.css';
 import SecondHeroSection from '../../Components/SecondHeroSection/SecondHeroSection';
+import EveryoneHolidays from '../../Components/EveryoneHolidays/EveryoneHolidays';
+import NeedInspirationSection from '../../Components/NeedInspiration/NeedInspirationSection';
 
 function Holidays() {
     return (
     <div className='holidays-page'>
         <SecondHeroSection secondHeroTitle='Your perfect holiday' />
+        <EveryoneHolidays />
+        <NeedInspirationSection />
     </div>
     )
 }
