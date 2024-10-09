0693c07f62671334a4e9d4e6b51f243859894e29
Morongwe Molaba <133514542+rongwes@users.noreply.github.com>
2024-10-09 03:46:59 +0000

    Added next holidays and recommended

--- src/Pages/Holidays/Holidays.jsx
+++ src/Pages/Holidays/Holidays.jsx
@@ -3,6 +3,9 @@ import './Holidays.css';
 import SecondHeroSection from '../../Components/SecondHeroSection/SecondHeroSection';
 import EveryoneHolidays from '../../Components/EveryoneHolidays/EveryoneHolidays';
 import NeedInspirationSection from '../../Components/NeedInspiration/NeedInspirationSection';
+import NextHoliday from '../../Components/NextHoliday/NextHoliday';
+import HolidayPlanSection from '../../Components/HolidayPlanSection/HolidayPlanSection';
+import RecommendedSection from '../../Components/RecommendedSection/RecommendedSection';
 
 function Holidays() {
     return (
@@ -10,6 +13,11 @@ function Holidays() {
         <SecondHeroSection secondHeroTitle='Your perfect holiday' />
         <EveryoneHolidays />
         <NeedInspirationSection />
+        <NextHoliday />
+        <HolidayPlanSection itemHolidayPlanTitle='Plan your summer holidays' />
+        <div className='mt-4 mt-sm-5'>
+            <RecommendedSection itemRecommendedTitle='Recommended for you' />
+        </div>
     </div>
     )
 }
