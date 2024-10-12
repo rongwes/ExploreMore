a6834e63c0124213e8cb0a9d9caec4d0a71d5fe5
Morongwe Molaba <133514542+rongwes@users.noreply.github.com>
2024-10-12 05:42:04 +0000

    Added Babel Plugin

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Home.css';
 import HeroSection from '../../Components/HeroSection/HeroSection';
-{/*import SearchForm from '../../Components/SearchForm/SearchForm';*/}
 import LastHolidaysSection from '../../Components/LastHolidaysSection/LastHolidaysSection';
 import RecommendedSection from '../../Components/RecommendedSection/RecommendedSection';
 import HolidayPlanSection from '../../Components/HolidayPlanSection/HolidayPlanSection';
 import FavouritesSection from '../../Components/FavouritesSection/FavouritesSection';
 import RecentHolidays from '../../Components/RecentHolidays/RecentHolidays';
 import NeedInspirationSection from '../../Components/NeedInspiration/NeedInspirationSection';
+{/*import SearchForm from '../../Components/SearchForm/SearchForm';*/}
 
 function Home() {
     return (
